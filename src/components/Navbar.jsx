6784c13b82721bbe78040250dc86289e8bad6419
waqar Ahmad <waqaar.ahmad@yahoo.com>
2021-08-28 22:59:25 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,107 @@
+import {
+  alpha,
+  AppBar,
+  Avatar,
+  Badge,
+  InputBase,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { Cancel, Mail, Notifications, Search } from "@material-ui/icons";
+import { useState } from "react";
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  logoLg: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  logoSm: {
+    display: "block",
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  search: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    borderRadius: theme.shape.borderRadius,
+    width: "50%",
+    [theme.breakpoints.down("sm")]: {
+      display: (props) => (props.open ? "flex" : "none"),
+      width: "70%",
+    },
+  },
+  input: {
+    color: "white",
+    marginLeft: theme.spacing(1),
+  },
+  cancel: {
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  searchButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  icons: {
+    alignItems: "center",
+    display: (props) => (props.open ? "none" : "flex"),
+  },
+  badge: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const classes = useStyles({ open });
+  return (
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6" className={classes.logoLg}>
+          Waqar Ahmad
+        </Typography>
+        <Typography variant="h6" className={classes.logoSm}>
+          WAQAR
+        </Typography>
+        <div className={classes.search}>
+          <Search />
+          <InputBase placeholder="Search..." className={classes.input} />
+          <Cancel className={classes.cancel} onClick={() => setOpen(false)} />
+        </div>
+        <div className={classes.icons}>
+          <Search
+            className={classes.searchButton}
+            onClick={() => setOpen(true)}
+          />
+          <Badge badgeContent={4} color="secondary" className={classes.badge}>
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="secondary" className={classes.badge}>
+            <Notifications />
+          </Badge>
+          <Avatar
+            alt="Waqar Ahmad"
+            src="https://media-exp1.licdn.com/dms/image/C4D03AQEale9j1FG-Vw/profile-displayphoto-shrink_100_100/0/1525712008835?e=1635379200&v=beta&t=79lfHtOnIrRmi153tDnuwpP-mP-98FatEAIhed5ELRk"
+          />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
